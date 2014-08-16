bf3b45959c87b1f98a4cf4dba50b790f04739558
oroce <robert@oroszi.net>
2014-08-16 09:50:30 +0200

    memory, temperature producers and selected sensors

--- client/index.js
+++ client/index.js
@@ -1,11 +1,18 @@
 var godot = require('godot');
 var sensortag = require('godot-sensortag');
+var temperature = require('./temperature');
+var memory = require('memory-producer');
 godot.createClient({
   producers: [
     sensortag({
       mappings: {
         'd6cd418cedf34c268062a0414019f285': 'test-sensortag'
+      },
+      tistOptions: {
+        sensors: ['irTemperature', 'humidity']
       }
-    })
+    }),
+    temperature(),
+    memory({})
   ]
 }).connect(1337);
