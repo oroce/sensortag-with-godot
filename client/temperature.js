3951e3c3f909db3bc74a93a6c3a73db2b3a0b085
oroce <robert@oroszi.net>
2014-08-16 09:49:37 +0200

    measure temp using vcgencmd

---
+++ client/temperature.js
@@ -0,0 +1,27 @@
+var producer = require('godot-producer');
+var vcgencmd;
+try{
+  vcgencmd = require('vcgencmd');
+} catch(x){
+  // probably process.arch !== 'arch'
+}
+
+module.exports = producer(
+  function ctor() {
+
+  },
+  function write() {
+    var temp;
+    if (!vcgencmd) {
+      return;
+    }
+    try{
+      temp = vcgencmd.measureTemp();
+      this.emit('data', {
+        value: temp
+      });
+    } catch(x){
+      this.emit('reactor:error', x);
+    }
+  }
+);
