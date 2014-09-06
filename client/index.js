47bc181828653017ad0fb43b90e66d0940b1c0f6
oroce <robert@oroszi.net>
2014-09-06 15:41:46 +0200

    added optional ttl, mappings, default params for temperature and memory

--- client/index.js
+++ client/index.js
@@ -7,16 +7,24 @@ var host = process.env.GODOT_SERVER||'localhost';
 godot.createClient({
   producers: [
     sensortag({
+      ttl: +process.env.TTL || 1000 * 15,
       mappings: {
-        'd6cd418cedf34c268062a0414019f285': 'st1',
-        '1aeaf513aeb54763ba51a0a7769683c2': 'st2'
+        'bc6a29ac9ad0': 'st2',
+        '1cba8c20e2c8': 'st1'
       },
       tistOptions: {
+        watch: false,
         sensors: ['irTemperature', 'humidity']
       }
     }),
-    temperature(),
-    memory({})
+    temperature({
+      host: 'rpi',
+      service: 'rpi/temperature'
+    }),
+    memory({
+      host: 'rpi',
+      service: 'rpi/memory'
+    })
   ]
 }).connect(port, host, function(err) {
   if (err) {
