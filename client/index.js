ff2bae548e9370c33baacb22aab971e396a46f8e
oroce <robert@oroszi.net>
2015-01-17 10:10:39 +0100

    initial

--- client/index.js
+++ client/index.js
@@ -4,8 +4,24 @@ var temperature = require('./temperature');
 var memory = require('memory-producer');
 var port = process.env.GODOT_PORT||1337;
 var host = process.env.GODOT_SERVER||'localhost';
+var os = require('os');
+var logger = require('logify')();
+
+if (process.env.LOGSTASH_HOST) {
+  logger.add(require('logify-logstash-transport'), {
+    host: process.env.LOGSTASH_HOST,
+    port: process.env.LOGSTASH_PORT,
+    transport: process.env.LOGSTASH_TRANSPORT || 'tcp'
+  });
+}
+
 godot.createClient({
   producers: [
+    godot.producer({
+      host: os.hostname(),
+      service: 'health/heartbeat',
+      ttl: 1000 * 15
+    }),
     sensortag({
       ttl: +process.env.TTL || 1000 * 15,
       mappings: {
@@ -17,18 +33,32 @@ godot.createClient({
         sensors: ['irTemperature', 'humidity']
       }
     }),
-    temperature({
+    /*temperature({
       host: 'rpi',
       service: 'rpi/temperature'
     }),
     memory({
       host: 'rpi',
       service: 'rpi/memory'
-    })
+    })*/
   ]
-}).connect(port, host, function(err) {
+})
+.on('reactor:error', function(err) {
+  logger.error(err, 'Reactor error: %s at %s', err, new Date());
+})
+.on('error', function(err) {
+  logger.error(err, 'Godot error %s at %s', err, new Date());
+})
+.connect(port, host, function(err) {
   if (err) {
-    console.error(err);
+    logger.error(err, 'Failed to connect: %s at %s', err, new Date());
   }
-  console.log('Connected to %s:%s', host, port);
+  logger.info('Connected to %s:%s at %s', host, port, new Date());
+});
+
+process.on('uncaughtException', function(err) {
+  logger.error(err, 'Uncaught exception: %s at %s', err, new Date());
+  process.nextTick(function() {
+    process.exit(1);
+  });
 });
--- server/index.js
+++ server/index.js
@@ -1,20 +1,89 @@
 var godot = require('godot');
 var influx = require('godot-influxdb');
+var nma = require('../lib/notify-my-android');
+function debug(socket, prefix) {
+  return socket.pipe(godot.console(function(d){
+    console.log(prefix, JSON.stringify(d));
+  }));
+}
+function store(socket) {
+  if (process.env.INFLUX_HOST) {
+    return socket.pipe(influx({
+      host: process.env.INFLUX_HOST||'localhost',
+      port: 8086,
+      user: '',
+      password: '',
+      database: 'test'
+    }));
+  }
+  return socket;
+}
+function notify(socket) {
+  if (process.env.SENDGRID_AUTH) {
+    console.log('sendgridding');
+    socket.pipe(godot.email({
+      auth: {
+        user: 'oroce',
+        key: process.env.SENDGRID_AUTH
+      },
+      from: 'godot@oroszi.net',
+      to: 'robert+sensortag@oroszi.net'
+    }));
+  }
+
+  if (process.env.NMA_KEY) {
+    console.log('NMAIng');
+    return socket
+      //.pipe(godot.change('state'))
+      .pipe(nma({
+        key: process.env.NMA_KEY
+      }));
+  }
+  //return lastOne;
+}
 var server = godot.createServer({
   type: 'udp',
   reactors: [
-    function (socket) {
-      return socket
-        .pipe(godot.console(function(d){
-          console.log(JSON.stringify(d));
-        }))
-        .pipe(influx({
-          host: 'localhost',
-          port: 8086,
-          user: '',
-          password: '',
-          database: 'test'
+
+    function heartbeat(socket) {
+      var where = socket
+        .pipe(godot.where('service', 'health/heartbeat'));
+      debug(where, '[heartbeat arrived]');
+
+      var expire = where
+        .pipe(godot.expire(1000*60))
+        .pipe(godot.map(function(data) {
+          data.state = 'expired';
+          return data;
         }));
+
+      debug(expire, '[heartbeat expired]');
+      notify(expire);
+      return expire;
+    },
+    function diag(socket) {
+      var where = socket
+        .pipe(godot.where('service', 'rpi/memory/*'));
+
+      debug(where, '[diagnosis]');
+      store(where);
+      return where;
+    },
+    function temperature(socket) {
+      var where = socket
+        .pipe(godot.where('service', 'temperature/*'));
+
+      debug(where, '[temperature]');
+      store(where);
+      return where;
+    },
+    function humidity(socket) {
+      var where = socket
+        .pipe(godot.where('service', 'humidity/*'));
+
+      debug(where, '[humidity]');
+      store(where);
+      return where;
     }
   ]
 }).listen(1337);
