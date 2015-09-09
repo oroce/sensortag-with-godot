bce36337b4d99221ffd8a258a7c20519d9584428
oroce <robert@oroszi.net>
2015-09-09 20:07:08 +0200

    upload flower power to cloud

---
+++ client/cloud.js
@@ -0,0 +1,101 @@
+var request = require('request');
+function auth(options, cb) {
+  request({
+    url: 'https://apiflowerpower.parrot.com/user/v1/authenticate',
+    qs: {
+      grant_type: 'password',
+      client_id: options.clientId,
+      client_secret: options.clientSecret,
+      username: options.username,
+      password: options.password
+    },
+    json: true
+  }, function(err, response, json) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, json.access_token);
+  });
+}
+
+module.exports.auth = auth;
+
+function get(options, cb) {
+  request({
+    url: 'https://apiflowöerpower.parrot.com/sensor_data/v2/sample/location/' + options.location,
+    headers: {
+      'Authorization': 'Bearer ' + options.token
+    },
+    qs: {
+      from_datetime_utc: options.from,
+      to_datetime_utc: options.to,
+    },
+    json: true
+  }, function(err, resp, json) {
+    if (err) {
+      return cb(err);
+    }
+
+    cb(null, json);
+  });
+}
+module.exports.get = get;
+
+function garden(options, cb) {
+  request({
+    url: 'https://apiflowerpower.parrot.com/sensor_data/v3/sync',
+    headers: {
+      'Authorization': 'Bearer ' + options.token
+    },
+    qs: {
+      include_s3_urls: 1
+    },
+    json: true
+  }, function(err, resp, json) {
+    if (err) {
+      return cb(err);
+    }
+
+    cb(null, json);
+  });
+}
+
+module.exports.garden = garden;
+
+function upload(options, cb) {
+  options.date = options.date || new Date();
+  var offset = (new Date()).getTimezoneOffset();
+  request({
+    method: 'PUT',
+    url: 'https://apiflowerpower.parrot.com/sensor_data/v5/sample',
+    headers: {
+      'Authorization': 'Bearer ' + options.token
+    },
+    body: {
+      'client_datetime_utc': options.date,
+      'user_config_version': options.userConfigVersion,
+      'tmz_offset': offset,
+      'session_histories': [{
+         'sensor_serial' : options.serial,
+         'session_id': options.currentId,
+         'sample_measure_period': options.currentSessionPeriod,
+         'sensor_startup_timestamp_utc': options.startupTime,
+         'session_start_index': options.currentSessionStartIdx
+      }],
+      'uploads': [{
+         'sensor_serial': options.serial,
+         'upload_timestamp_utc': options.date,
+         'buffer_base64': options.history,
+         'app_version': '',
+         'sensor_fw_version': '',
+         'sensor_hw_identifier' : '',
+      }]
+    },
+    json: true
+  }, function(err, resp, body) {
+    console.log(err, body);
+    cb(err, body);
+  });
+}
+
+module.exports.upload = upload;
--- client/flower-power-history.js
+++ client/flower-power-history.js
@@ -2,90 +2,143 @@
 var producer = require('godot-producer');
 var FlowerPower = require('flower-power');
 var series = require('run-series');
-module.exports = producer(function ctor() {
-  this.uuid = options.uuid;
+var waterfall = require('run-waterfall');
+var debug = require('debug')('swg:device:flower-power-history');
+require('./discover')(FlowerPower);
+var Producer = producer(function ctor() {
+  var uuid = this.uuid = options.uuid;
+  debug('initialized flower power with %s', this.uuid || '<empty uuid>');
+  this.filter = function(device) {
+    if (!uuid) {
+      debug('filtering %s, but no filter', device.uuid);
+      this.onDiscover(device);
+      return;
+    }
+    debug('filtering device: "%s" <=> "%s"', device.uuid, uuid);
+    if (device.uuid === uuid) {
+      this.onDiscover(device);
+    }
+  }.bind(this);
+  this.on('error', console.error.bind(console));
 }, function produce() {
-  var self = this;
-  if (this._stillProducing) {
-    //
+  debug('producing, stopping and restarting discovery');
+  FlowerPower.stopDiscoverThis(this.filter);
+  if (this.device) {
+    this.device.disconnect();
+    this.device = null;
   }
-  this._stillProducing = true;
-  if (this.uuid) {
-    FlowerPower.discoverByUuid(this.uuid, onDiscover);
-  } else {
-    FlowerPower.discover(onDiscover);
-  }
+  FlowerPower.discoverThis(this.filter);
+});
+module.exports = Producer;
 
-  function onDiscover(flowerPower) {
-    var lastEntryIdx;
-    var currentSessionID;
-    var currentSessionStartIdx;
-    var currentSessionPeriod;
-    var startIdx;
-    series([
-      function(cb) {
-        flowerPower.connectAndSetup(cb),
-      },
-      function(cb) {
-        flowerPower.getHistoryLastEntryIdx(function(err, data) {
-          lastEntryIdx = data;
-          cb(err);
+Producer.prototype.onDiscover = function onDiscover(flowerPower) {
+  var options = this.options;
+  var lastEntryIdx;
+  var currentSessionID;
+  var currentSessionStartIdx;
+  var currentSessionPeriod;
+  var startIdx;
+  series([
+    function(cb) {
+      flowerPower.connectAndSetup(cb);
+    },
+    function(cb) {
+      flowerPower.getHistoryLastEntryIdx(function(err, data) {
+        lastEntryIdx = data;
+        cb(err);
+      });
+    },
+    function(cb) {
+      flowerPower.getHistoryCurrentSessionID(function(err, data) {
+        currentSessionID = data;
+        cb(err);
+      });
+    },
+    function(cb) {
+      flowerPower.getHistoryCurrentSessionStartIdx(function(err, data) {
+        currentSessionStartIdx = data;
+        cb(err);
+      });
+    },
+    function(cb) {
+      flowerPower.getHistoryCurrentSessionPeriod(function(err, data) {
+        currentSessionPeriod = data;
+        cb(err);
+      });
+    },
+    function(cb) {
+      flowerPower.getStartupTime(cb);
+    },
+    function(cb) {
+      startIdx = lastEntryIdx - 200;
+      flowerPower.getHistory(startIdx, cb);
+    },
+  ], function(err, data) {
+    if (err) {
+      self.emit('error', err);
+      // do not return, maybe we have some data
+    }
+    data = data || [];
+    // find mapping of data
+    var startupTime = data[5];
+    var history = data[6];
+    var time = new Date();
+    if (!history) {
+      debug('Failed to get history');
+      return;
+    }
+    var offset = time.getTimezoneOffset();
+    self.emit('data', {
+      service: 'flowerpower/history',
+      meta: {
+        history: history,
+        startupTime: startupTime
+      }
+    });
+    waterfall([
+      cloud.auth.bind(cloud, {
+        clientId: options.clientId,
+        clientSecret: options.clientSecret,
+        username: options.username,
+        password: options.password
+      }),
+      /*function(token, cb) {
+        cloud.garden({
+          token: token
+        }, function(err, params) {
+          if (err) {
+            return cb(err);
+          }
+          params.token = token;
+          cb(null, params);
         });
-      },
-      function(cb) {
-        flowerPower.getHistoryCurrentSessionID(function(err, data) {
-          currentSessionID = data;
-          cb(err);
-        });
-      },
-      function(cb) {
-        flowerPower.getHistoryCurrentSessionStartIdx(function(err, data) {
-          currentSessionStartIdx = data;
-          cb(err);
-        });
-      },
-      function(cb) {
-        flowerPower.getHistoryCurrentSessionPeriod(function(err, data) {
-          currentSessionPeriod = data;
-          cb(err);
-        });
-      },
-      function(cb) {
-        flowerPower.getStartupTime(cb);
-      },
-      function(cb) {
-        startIdx = lastEntryIdx - 200;
-        flowerPower.getHistory(startIdx, cb);
-      },
-    ], function(err, data) {
+      },*/
+      function(params, cb) {
+        params.history = history;
+        params.currentId = currentSessionID;
+        params.startupTime = startupTime;
+        params.currentSessionStartIdx = currentSessionStartIdx;
+        params.currentSessionPeriod = currentSessionPeriod;
+        params.userConfigVersion = 8;
+        params.serial = 'A0143D000008DC92'; // wtf why no device.uuid
+        cloud.upload(params, cb);
+      }
+    ], function(err, result) {
       if (err) {
         self.emit('error', err);
-        // do not return, maybe we have some data
+        self.emit('data', {
+          service: 'flowerpower/history-uploaded',
+          state: 'error',
+          meta: err
+        });
+        return;
       }
-      data = data || [];
-      // find mapping of data
-      var startupTime = data[5];
-      var history = data[6];
-      var time = new Date();
-      var offset = time.getTimezoneOffset();
+      console.log(result);
       self.emit('data', {
-        service: 'flowerpower/history',
-        meta: {
-          history: history,
-          startupTime: startupTime
-        }
+        service: 'flowerpower/history-uploaded',
+        state: 'ok',
+        meta: result
       });
-      series([
-        cloud.auth,
-        cloud.garden,
-        cloud.upload
-      ], function(err, result) {
-        if (err) {
-          return self.emit('error', err);
-        }
-      });
-      delete self._stillProducing;
     });
-
-  };
-});
+  });
+};
