7bbd2efc4e401310f799161d577a0f28dd559024
oroce <robert@oroszi.net>
2015-10-10 13:01:43 +0200

    add link to source how to detect uptime

--- client/uptime/linux.js
+++ client/uptime/linux.js
@@ -1,4 +1,5 @@
 'use strict';
+// source: http://unix.stackexchange.com/a/165014
 var execSync = require('child_process').execSync;
 var epoch = execSync('date -d "$(who -b | awk \'{print $4,$3}\' | tr - / )" +%s');
 module.exports = epoch * 1000;
